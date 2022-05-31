8796bd56a646a25de1c801e4865a1ff1d1edd538
James Nelson <jnelso89@email.tjc.edu>
2022-05-31 00:07:19 -0400

    Update

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -501,7 +501,7 @@ def _mirror(bot, update, isZip=False, extract=False, isQbit=False, isLeech=False
         if len(mesg) > 2:
             msg += '\n' + mesg[1] + '\n' + mesg[2]
         nextmsg = sendMessage(msg, bot, nextmsg)
-        nextmsg.from_user.id = update.message.from_user.id
+        #nextmsg.from_user.id = update.message.from_user.id
         multi -= 1
         time.sleep(4)
         _mirror(bot, nextmsg, isZip, extract, isQbit, isLeech, multi)
