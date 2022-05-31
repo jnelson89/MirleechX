bc3072c35eddbcad70bdc5135d9a0bcf8b6edb8d
James Nelson <jnelso89@email.tjc.edu>
2022-05-31 14:11:52 -0400

    Update

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -491,16 +491,12 @@ def _mirror(bot, update, isZip=False, extract=False, isQbit=False, isLeech=False
     
     if multi > 1:
         time.sleep(4)
-        nextmsg = type('nextmsg', (object, ), 
-        {
-        'chat_id': update.message.chat_id, 
-        'message_id': update.message.reply_to_message.message_id + 1
-        }
-        )
+        ms = update
+        nextmsg = type('nextmsg', (object, ), {'chat_id': message.chat_id, 'message_id': message.reply_to_message.message_id + 1})
         msg = message_args[0]
         if len(mesg) > 2:
             msg += '\n' + mesg[1] + '\n' + mesg[2]
-        nextmsg = sendMessage(msg, bot, nextmsg)
+        nextmsg = sendMessage(msg, bot, ms.nextmsg)
         nextmsg.from_user.id = update.message.from_user.id
         multi -= 1
         time.sleep(4)
