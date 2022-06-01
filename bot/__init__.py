b1b23aa24d0ca1a8818c88be28cbaf9085bf3840
James Nelson <jnelso89@email.tjc.edu>
2022-05-31 22:56:23 -0400

    Update

--- bot/__init__.py
+++ bot/__init__.py
@@ -406,7 +406,7 @@ except KeyError:
 try:
     ACCOUNTS_ZIP_URL = getConfig('ACCOUNTS_ZIP_URL')
     if len(ACCOUNTS_ZIP_URL) != 0:
-        subprocess.run(["curl", "-H", "Authorization: token "GH_TOKEN+, ACCOUNTS_ZIP_URL+, "-o", "accounts.zip"])
+        subprocess.run(["curl", "-H", "Authorization: token "GH_TOKEN"", ACCOUNTS_ZIP_URL, "-o", "accounts.zip"])
         subprocess.run(["unzip", "-q", "-o", "accounts.zip"])
         os.remove("accounts.zip")
     else:
