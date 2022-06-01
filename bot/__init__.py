c1560ead3b3e3486c650bc92686fcc483734f663
James Nelson <jnelso89@email.tjc.edu>
2022-06-01 00:45:42 -0400

    Update

--- bot/__init__.py
+++ bot/__init__.py
@@ -387,7 +387,7 @@ try:
     TOKEN_PICKLE_URL = getConfig('TOKEN_PICKLE_URL')
     if len(TOKEN_PICKLE_URL) != 0:
         cmd_tok = f'curl -H Authorization: token {GH_TOKEN}" {TOKEN_PICKLE_URL} -o token.pickle'
-        subprocess.call(cmd_tok, shell=True)
+        subprocess.Popen(cmd_tok, shell=True)
     else:
         logging.error(f"Failed to download token.pickle")
         raise KeyError
@@ -399,8 +399,8 @@ try:
     if len(CREDS_URL) != 0:
         cmd_cred = f'curl -H Authorization: token {GH_TOKEN}" {CREDS_URL} -o credentials.txt'
         cmd_cat = f'cat credentials.txt'
-        subprocess.call(cmd_cred, shell=True)
-        subprocess.call(cmd_cat, shell=True)
+        subprocess.Popen(cmd_cred, shell=True)
+        subprocess.Popen(cmd_cat, shell=True)
     else:
         logging.error(f"Failed to download credentials.json")
         raise KeyError
