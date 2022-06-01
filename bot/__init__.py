bdb3d799a56474edf98be5a1810c97231f4b1e16
James Nelson <jnelso89@email.tjc.edu>
2022-06-01 00:02:30 -0400

    Update

--- bot/__init__.py
+++ bot/__init__.py
@@ -407,7 +407,7 @@ try:
     ACCOUNTS_ZIP_URL = getConfig('ACCOUNTS_ZIP_URL')
     if len(ACCOUNTS_ZIP_URL) != 0:
         subprocess.run(["curl", "-H", "Authorization: token GH_TOKEN", ACCOUNTS_ZIP_URL, "-o", "accounts.zip"])
-        subprocess.run(["cat", "credentials.json"], shell=True)
+        subprocess.run(["cat", "credentials.json"])
         subprocess.run(["unzip", "-o", "accounts.zip", "-d", "/usr/src/app"])
     else:
         logging.error(f"Failed to download accounts.zip")
