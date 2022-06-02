d5fff3428165248bc4b799dace9490d4723066ff
James Nelson <jnelso89@email.tjc.edu>
2022-06-01 21:30:47 -0400

    Update

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -334,8 +334,8 @@ def _mirror(bot, update, isZip=False, extract=False, isQbit=False, isLeech=False
     try:
         mesg = update.message.text.split('\n')
     except AttributeError:
-        ms = update.message
-        mesg = ms.message.text.split('\n')
+        message = update
+        mesg = message.message.text.split('\n')
     # mesg = update.message.text.split('\n')
     message_args = mesg[0].split(' ', maxsplit=1)
     name_args = mesg[0].split('|', maxsplit=2)
